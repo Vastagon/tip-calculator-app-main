6202ff6f4c2ab025c3d790f54fbf6e787802f0ee
Vastagon <77780118+Vastagon@users.noreply.github.com>
2021-09-19 15:12:02 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -20,30 +20,37 @@ let totalPerPerson = 0.00.toFixed(2);
 bill.addEventListener('keyup', function(event){//Save the bill total after pressing enter to billTotal
         billTotal = parseInt(bill.value);
         tipAmount = (billTotal * percentage) / peopleNum;
-        totalPerPerson = (tipAmount + billTotal) / peopleNum;
+        totalPerPerson = tipAmount + (billTotal / peopleNum);
 
-        shownTipAmount.textContent = "$" + tipAmount.toFixed(2);
-        shownTotalAmount.textContent = "$" + totalPerPerson.toFixed(2);    
+        if(billTotal > 0 && percentage > 0 && peopleNum > 0){
+            shownTipAmount.textContent = "$" + tipAmount.toFixed(2);
+            shownTotalAmount.textContent = "$" + totalPerPerson.toFixed(2);
+        }  
 })
 
 numberOfPeople.addEventListener('keyup', function(event){//Saves the number of people after pushing enter to peopleNum
         peopleNum = parseInt(numberOfPeople.value);
 
         tipAmount = ((billTotal * percentage) / peopleNum);
-        totalPerPerson = (tipAmount + billTotal) / peopleNum;
+        totalPerPerson = tipAmount + (billTotal / peopleNum);
 
-        shownTipAmount.textContent = "$" + tipAmount.toFixed(2);
-        shownTotalAmount.textContent = "$" + totalPerPerson.toFixed(2);   
+        if(billTotal > 0 && percentage > 0 && peopleNum > 0){
+            shownTipAmount.textContent = "$" + tipAmount.toFixed(2);
+            shownTotalAmount.textContent = "$" + totalPerPerson.toFixed(2);
+        }  
 })
 
 customPercent.addEventListener('keyup', function(event){//Saves the percentage after pushing enter to percentage
         percentage = customPercent.value / 100;
 
         tipAmount = ((billTotal * percentage) / peopleNum);
-        totalPerPerson = (tipAmount + billTotal) / peopleNum;
+        totalPerPerson = tipAmount + (billTotal / peopleNum);
 
-        shownTipAmount.textContent = "$" + tipAmount.toFixed(2);
-        shownTotalAmount.textContent = "$" + totalPerPerson.toFixed(2);
+        if(billTotal > 0 && percentage > 0 && peopleNum > 0){
+            shownTipAmount.textContent = "$" + tipAmount.toFixed(2);
+            shownTotalAmount.textContent = "$" + totalPerPerson.toFixed(2);
+        }
+
    
 })
 
@@ -57,53 +64,55 @@ resetButton.addEventListener('click', function(){//Reset button reloads the page
 function setPercent5(){
     percentage = .05;
     tipAmount = ((billTotal * percentage) / peopleNum);
-    totalPerPerson = (tipAmount + billTotal) / peopleNum;
+    totalPerPerson = tipAmount + (billTotal / peopleNum);
 
-    shownTipAmount.textContent = "$" + tipAmount.toFixed(2);
-    shownTotalAmount.textContent = "$" + totalPerPerson.toFixed(2);
+    if(billTotal > 0 && percentage > 0 && peopleNum > 0){
+        shownTipAmount.textContent = "$" + tipAmount.toFixed(2);
+        shownTotalAmount.textContent = "$" + totalPerPerson.toFixed(2);
+    }
 }
 function setPercent10(){
     percentage = .10;
     tipAmount = ((billTotal * percentage) / peopleNum);
-    totalPerPerson = (tipAmount + billTotal) / peopleNum;
+    totalPerPerson = tipAmount + (billTotal / peopleNum);
 
-    shownTipAmount.textContent = "$" + tipAmount.toFixed(2);
-    shownTotalAmount.textContent = "$" + totalPerPerson.toFixed(2);
+    if(billTotal > 0 && percentage > 0 && peopleNum > 0){
+        shownTipAmount.textContent = "$" + tipAmount.toFixed(2);
+        shownTotalAmount.textContent = "$" + totalPerPerson.toFixed(2);
+    }
 }
 function setPercent15(){
     percentage = .15;
     tipAmount = ((billTotal * percentage) / peopleNum);
-    totalPerPerson = (tipAmount + billTotal) / peopleNum;
+    totalPerPerson = tipAmount + (billTotal / peopleNum);
 
-    shownTipAmount.textContent = "$" + tipAmount.toFixed(2);
+    if(billTotal > 0 && percentage > 0 && peopleNum > 0){
+        shownTipAmount.textContent = "$" + tipAmount.toFixed(2);
         shownTotalAmount.textContent = "$" + totalPerPerson.toFixed(2);
+    }
 }
 function setPercent25(){
     percentage = .25;
     tipAmount = ((billTotal * percentage) / peopleNum);
-    totalPerPerson = (tipAmount + billTotal) / peopleNum;
+    totalPerPerson = tipAmount + (billTotal / peopleNum);
 
-    shownTipAmount.textContent = "$" + tipAmount.toFixed(2);
-    shownTotalAmount.textContent = "$" + totalPerPerson.toFixed(2);
+    if(billTotal > 0 && percentage > 0 && peopleNum > 0){
+        shownTipAmount.textContent = "$" + tipAmount.toFixed(2);
+        shownTotalAmount.textContent = "$" + totalPerPerson.toFixed(2);
+    }
 }
 function setPercent50(){
     percentage = .50;
     tipAmount = ((billTotal * percentage) / peopleNum);
-    totalPerPerson = (tipAmount + billTotal) / peopleNum;
+    totalPerPerson = tipAmount + (billTotal / peopleNum);
 
-    shownTipAmount.textContent = "$" + tipAmount.toFixed(2);
-    shownTotalAmount.textContent = "$" + totalPerPerson.toFixed(2);
+    if(billTotal > 0 && percentage > 0 && peopleNum > 0){
+        shownTipAmount.textContent = "$" + tipAmount.toFixed(2);
+        shownTotalAmount.textContent = "$" + totalPerPerson.toFixed(2);
+    }
 }
 fivePercent.addEventListener("click", setPercent5);
 tenPercent.addEventListener("click", setPercent10);
 fifteenPercent.addEventListener("click", setPercent15);
 twentyFivePercent.addEventListener("click", setPercent25);
 fiftyPercent.addEventListener("click", setPercent50);
-
-
-
-
-
-
-
-
